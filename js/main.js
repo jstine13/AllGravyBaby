df782c5b9462f44f3961332105f28f7c410478a5
Joshua Stine <jstine13@ivytech.edu>
2025-07-08 17:21:51 -0400

    add order button on scroll to index. added classes and id's to random elements for css purposes.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	window.addEventListener("scroll", function () {
 		if (main.getBoundingClientRect().top <= 2) {
-		    backToTopBtn.classList.add("show");
+		  backToTopBtn.classList.add("show");
 		} else {
 			backToTopBtn.classList.remove("show");
 		}
@@ -13,4 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
 	backToTopBtn.addEventListener("click", function () {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const orderNowBtn = document.getElementById("orderPopUp");
+  const graviesSection = document.querySelector("#gravyShowcase");
+
+  window.addEventListener("scroll", function () {
+    if (graviesSection.getBoundingClientRect().top <= 25) {
+      orderNowBtn.classList.add("show");
+    } else {
+      orderNowBtn.classList.remove("show");
+    }
+  });
 });
