8ce2df4106f438519c325c3272c33bbbaea31dd3
Joshua Stine <jstine13@ivytech.edu>
2025-07-14 23:01:55 -0400

    peer review checklist updates: accessibility, SEO, alt text, CTA, and responsive fixes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const backToTopBtn = document.getElementById("backToTopBtn");
-	const main = document.querySelector("main");
-
-	window.addEventListener("scroll", function () {
-		if (main.getBoundingClientRect().top <= 1) {
-		  backToTopBtn.classList.add("show");
-		} else {
-			backToTopBtn.classList.remove("show");
-		}
-	});
-
-	backToTopBtn.addEventListener("click", function () {
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	});
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+  const backToTopBtn = document.getElementById("backToTopBtn");
   const orderNowBtn = document.getElementById("orderPopUp");
-  const caughtOrder = document.querySelector("#orderCatcher");
+  const main = document.querySelector("main");
+  const caughtOrder = document.querySelector("main");
 
   window.addEventListener("scroll", function () {
+    if (main.getBoundingClientRect().top <= 1) {
+      backToTopBtn.classList.add("show");
+    } else {
+      backToTopBtn.classList.remove("show");
+    }
     if (caughtOrder.getBoundingClientRect().top <= 1) {
       orderNowBtn.classList.add("show");
     } else {
       orderNowBtn.classList.remove("show");
     }
+  });
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
